2a4e1aeee2d9e71ad0f3a4602b78a810df4319e5
“Tengereeb8” <tengererdenebattulga8@gmail.com>
2026-02-13 10:10:28 +0800

    ready to show

--- components/LetterInterface.jsx
+++ components/LetterInterface.jsx
@@ -18,7 +18,7 @@ export default function LetterInterface({ onClaim }) {
        
         
         <div className="max-w-prose mx-auto">
-            <h1 className="font-cursive text-5xl mb-8">Dearest Naraa,</h1>
+            <h1 className="font-cursive text-5xl mb-8">Dear Khongorzul</h1>
             
             <div className="space-y-6 text-lg leading-relaxed font-sans text-stone-800">
                 <p>
@@ -26,23 +26,21 @@ export default function LetterInterface({ onClaim }) {
                     they seem to fall short of what my heart truly holds for you.
                 </p>
                 <p>
-                    From the moment we met, you became my favorite thought, my safest place, 
+                    From the moment we started conversation, you became my favorite thought, my safest place, 
                     and my greatest adventure. You listen to my dreams and hold my fears 
                     with such gentleness.
                 </p>
                 <p>
                     Thank you for being my peace, my joy, and my greatest supporter. 
-                    I promise to cherish you, today and alway.
+                    I promise to cherish you, today and always.
                 </p>
                 <p>
-                  "In all the world, there is no heart for me like yours. 
-                  In all the world, there is no love for you like mine."
-                </p>
+                “I may have never imagined this before, but I truly hope I can be the one for you — the person you choose to love.”</p>
             </div>
 
             <div className="mt-12 text-right">
-                <p className="font-cursive text-3xl">Forever Yours,</p>
-                <p className="mt-2 font-bold">[Crown Tulgaa]</p>
+                <p className="mt-2 font-bold">From:</p>
+                <p className="mt-2 font-bold">[Tenger-Erdene]</p>
             </div>
             
             <div className="mt-16 flex justify-center">
@@ -62,13 +60,18 @@ export default function LetterInterface({ onClaim }) {
         
         {/* Mock Polaroids */}
         {[
-            { id: 1, caption: "Crown Tulgaa", color: "bg-blue-200", image: '/tulgaa.png' }, 
-            { id: 2, caption: "Crown Naraa", color: "bg-orange-200", image: '/naraa.png' },
-            { id: 3, caption: "Our Wedding", color: "bg-indigo-200", image: '/wedding.png' }
+            { id: 1, caption: "C", color: "bg-blue-200", image: '/h1.jpeg' }, 
+            { id: 2, caption: "Crown Naraa", color: "bg-orange-200", image: '/h2c.jpeg' },
+            { id: 3, caption: "Our Wedding", color: "bg-indigo-200", image: '/h3.jpeg' }
         ].map((item) => (
             <div key={item.id} className="bg-white p-3 shadow-lg transform rotate-2 hover:rotate-0 transition-transform duration-300">
-                <div className={`w-full h-48 ${item.color} mb-3 flex items-center justify-center text-gray-500`}>
-                  <Image src={item.image} alt={item.caption} width={200} height={170} />
+                  <div className={`relative w-full h-46 ${item.color} mb-3 flex items-center justify-center`}>
+                <Image
+  src={item.image}
+  alt={item.caption}
+  fill
+  className="object-contain"
+/>
                 </div>
                 <p className="text-center font-cursive text-xl text-stone-700">{item.caption}</p>
             </div>
